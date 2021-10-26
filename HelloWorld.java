978d4f2ad57f58f85dded8d700e3aa7a10650c19
roystonjord <39296028+roystonjord@users.noreply.github.com>
2021-10-26 08:12:39 +0300

    new commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!123456"); 
-        System.out.println("Hello, World!223"); 
+        System.out.println("Hello, World!1"); 
+        System.out.println("Hello, World!2"); 
     }
 }
